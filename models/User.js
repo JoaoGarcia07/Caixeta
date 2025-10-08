1cac1ff1e21fb157381e6d7fda3872e8c83c607d
JoaoGarcia07 <joaovitorprestesgarcia@gmail.com>
2025-10-08 20:52:39 -0300

    Integrando com o banco

--- models/User.js
+++ models/User.js
@@ -1,26 +1,25 @@
-// models/User.js
-
-const { DataTypes } = require('sequelize');
-const sequelize = require('../config/database'); // Importa nossa conexão
-
-const User = sequelize.define('User', {
-  // Definimos as colunas da nossa tabela 'users'
-  name: {
-    type: DataTypes.STRING,
-    allowNull: false // O nome não pode ser nulo
-  },
-  email: {
-    type: DataTypes.STRING,
-    allowNull: false,
-    unique: true // O email deve ser único
-  },
-  password: {
-    type: DataTypes.STRING,
-    allowNull: false
+'use strict';
+const {
+  Model
+} = require('sequelize');
+module.exports = (sequelize, DataTypes) => {
+  class User extends Model {
+    /**
+     * Helper method for defining associations.
+     * This method is not a part of Sequelize lifecycle.
+     * The `models/index` file will call this method automatically.
+     */
+    static associate(models) {
+      // define association here
+    }
   }
-}, {
-  // Opções adicionais do model
-  tableName: 'users' // Garante que o nome da tabela seja 'users'
-});
-
-module.exports = User;
+  User.init({
+    email: DataTypes.STRING,
+    name: DataTypes.STRING,
+    password: DataTypes.STRING
+  }, {
+    sequelize,
+    modelName: 'User',
+  });
+  return User;
+};
